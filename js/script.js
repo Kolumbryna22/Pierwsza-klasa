3a170f4d47388419155ba1f9a9bff07dfd9a6ab7
Dominik Wilusz <wilusz1@o2.pl>
2018-08-18 16:01:09 +0200

    Add class

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(function() {
+    'use strict';
+
+    var iPhone9 = new Phone('Apple', 4500, 'silver');
+    var SamsungGalaxyS9 = new Phone('Samsung', 3000, 'Midnight Black');
+    var HuaweiP20Pro = new Phone('Huawei', 3500, 'silver');
+
+    function Phone(brand, price, color) {
+        this.brand = brand;
+        this.price = price;
+        this.color = color;
+
+        this.printInfo = function() {
+            console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.');
+        };
+    };
+
+    iPhone9.printInfo();
+    SamsungGalaxyS9.printInfo();
+    HuaweiP20Pro.printInfo();
+});
